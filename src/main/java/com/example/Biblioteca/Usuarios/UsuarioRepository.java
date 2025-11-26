ab205f323c2db5771a3a343da7a3e4d2d40bef8a
bjusty11 <justibruno10@gmail.com>
2025-11-26 13:27:50 -0300

    feat: Fazendo a ligacao com banco de dados H2

--- src/main/java/com/example/Biblioteca/Usuarios/UsuarioRepository.java
+++ src/main/java/com/example/Biblioteca/Usuarios/UsuarioRepository.java
@@ -3,4 +3,5 @@ package com.example.Biblioteca.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
 }
