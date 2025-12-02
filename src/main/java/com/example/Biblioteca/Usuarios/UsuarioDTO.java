e0c48bf5946530619af099a1dbe376e084435b66
bjusty11 <justibruno10@gmail.com>
2025-12-02 09:34:09 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no UsuarioMapper os caminhos entidade X DTO

--- src/main/java/com/example/Biblioteca/Usuarios/UsuarioDTO.java
+++ src/main/java/com/example/Biblioteca/Usuarios/UsuarioDTO.java
@@ -1,4 +1,21 @@
 package com.example.Biblioteca.Usuarios;
+import com.example.Biblioteca.Book.BookModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
-}
+
+        private Long id;
+        private String nome;
+        private String imgUrl;
+        private int idade;
+        private String email;
+        private String cpf;
+        private BookModel books;
+
+    }
+
--- src/main/java/com/example/Biblioteca/Usuarios/UsuarioMapper.java
+++ src/main/java/com/example/Biblioteca/Usuarios/UsuarioMapper.java
@@ -1,4 +1,37 @@
 package com.example.Biblioteca.Usuarios;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuarioMapper {
+
+    // Mapeamento de uma entity para um DTO
+
+    public UsuarioModel map(UsuarioDTO usuarioDTO) {
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setId(usuarioDTO.getId());
+        usuarioModel.setNome(usuarioDTO.getNome());
+        usuarioModel.setEmail(usuarioDTO.getEmail());
+        usuarioModel.setCpf(usuarioDTO.getCpf());
+        usuarioModel.setIdade(usuarioDTO.getIdade());
+        usuarioModel.setImgUrl(usuarioDTO.getImgUrl());
+        usuarioModel.setBooks(usuarioDTO.getBooks());
+
+        return usuarioModel;
+    }
+
+    // Mapeamento de um DTO para uma entity
+
+    public UsuarioDTO map(UsuarioModel usuarioModel) {
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(usuarioModel.getId());
+        usuarioDTO.setNome(usuarioModel.getNome());
+        usuarioDTO.setEmail(usuarioModel.getEmail());
+        usuarioDTO.setCpf(usuarioModel.getCpf());
+        usuarioDTO.setIdade(usuarioModel.getIdade());
+        usuarioDTO.setImgUrl(usuarioModel.getImgUrl());
+        usuarioDTO.setBooks(usuarioModel.getBooks());
+
+        return usuarioDTO;
+    }
 }
