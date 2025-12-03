2a825b75577638f66e91cee284defe41200916d3
bjusty11 <justibruno10@gmail.com>
2025-12-03 09:39:27 -0300

    refactor: Refatora o controller para incluir o ResponseEntity nas minhas rotas e customizar as respostas das requisições dos clientes.

--- src/main/java/com/example/Biblioteca/Usuarios/UsuarioController.java
+++ src/main/java/com/example/Biblioteca/Usuarios/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.example.Biblioteca.Usuarios;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,39 +11,60 @@ import java.util.List;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-    private UsuarioService  usuarioService;
+    private UsuarioService usuarioService;
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
 
-        //Adicionar usuário (CREATE)
-        @PostMapping("/criar")
-        public UsuarioDTO criarUsuario(@RequestBody UsuarioDTO usuario) {
-            return usuarioService.criarUsuario(usuario);
-        }
+    //Adicionar usuário (CREATE)
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarUsuario(@RequestBody UsuarioDTO usuario) {
+        UsuarioDTO novoUsuario = usuarioService.criarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Usuário criado com sucesso: " + novoUsuario.getNome() + " (ID): " + novoUsuario.getId());
+    }
 
-        //Mostrar todos os usuarios (READ)
-        @GetMapping("/listar")
-        public List<UsuarioDTO> listarUsuarios() {
-            return usuarioService.listarUsuarios();
-        }
+    //Mostrar todos os usuarios (READ)
+    @GetMapping("/listar")
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
+        List<UsuarioDTO> usuarios = usuarioService.listarUsuarios();
+        return ResponseEntity.ok(usuarios);
+    }
 
-        //Mostrar usuario por ID (READ)
-        @GetMapping("/listar/{id}")
-        public UsuarioDTO listarUsuarioPorId(@PathVariable Long id) {
-            return usuarioService.listarUsuarioPorId(id);
+    //Mostrar usuario por ID (READ)
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarUsuarioPorId(@PathVariable Long id) {
+        UsuarioDTO usuario = usuarioService.listarUsuarioPorId(id);
+        if (usuario != null) {
+            return ResponseEntity.ok(usuario);
+        } else  {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Usuário com o (ID) " + id + " não foi encontrado.");
         }
+    }
 
-        //Alterar dados dos usuarios (UPDATE)
-        @PutMapping("/alterar/{id}")
-        public UsuarioDTO atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado) {
-            return usuarioService.atualizarUsuario(id, usuarioAtualizado);
+    //Alterar dados dos usuarios (UPDATE)
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado) {
+        UsuarioDTO usuario = usuarioService.atualizarUsuario(id, usuarioAtualizado);
+        if (usuario != null) {
+            return ResponseEntity.ok(usuario);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Usuário com o (ID) " + id + " não foi encontrado.");
         }
+    }
 
-        //Deletar usuário (DELETE)
-        @DeleteMapping("/deletar/{id}")
-        public void deletarUsuarioPorId(@PathVariable Long id) {
+    //Deletar usuário (DELETE)
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarUsuarioPorId(@PathVariable Long id) {
+        if (usuarioService.listarUsuarioPorId(id) != null) {
             usuarioService.deletarUsuarioPorId(id);
+            return ResponseEntity.ok("Usuário deletado com sucesso!" + id + "Deletado com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Usuário com o (ID) " + id + " não foi encontrado.");
         }
+    }
 }
